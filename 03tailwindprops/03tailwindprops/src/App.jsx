b53d43b6c0a68dc2a416cc961632de91c5cf422d
Farhan-prottoy <farhan.prottoy.17@gmail.com>
2025-12-20 23:52:29 +0600

    learned props and used tailwindcss in react(vite)(fixed some issues

--- 03tailwindprops/03tailwindprops/src/App.jsx
+++ 03tailwindprops/03tailwindprops/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-  let MyObj = { Name: "Prottoy", Age: 25 };
   let secondObj = { Name: "Protoy", Age: 25 };
   let Arr = [1, 2, 3];
   let username = "Prottoy22";
